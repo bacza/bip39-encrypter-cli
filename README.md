2656d755a88b5824a05a1c013ba202c581ad34a3
Jaroslaw Baczynski <jbaczynski@gmail.com>
2024-03-06 20:55:33 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# bip39-encrypter-cli
+
+This is a CLI utility for [bip39-encrypter](https://www.npmjs.com/package/bip39-encrypter) library package.
+
+## Installation
+
+The package can be installed globally or just ran directly with `npx`.
+
+```sh
+npm install -g bip39-encrypter-cli
+```
+
+## Usage
+
+If installed globally, just say:
+
+```sh
+bip39-encrypter-cli
+```
+
+If using `npx`, invoke:
+
+```sh
+npx bip39-encrypter-cli
+```
+
+Then follow on-screen instructions.
+
+## Disclaimer
+
+This is a hobby project, no warranty of any kind is provided. Do NOT use it at home unless you know what you are doing!
+
+## License
+
+Copyright (c) 2024 Jaroslaw Baczynski
+
+Licensed under the [MIT license](LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,179 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import fig from 'figures';
+import { input, password, select } from '@inquirer/prompts';
+import autocomplete from 'inquirer-autocomplete-standalone';
+import { Encrypter, WORDS } from 'bip39-encrypter';
+import pkg from '../package.json';
+
+const CLEAR_PROMPT = { clearPromptOnDone: true };
+
+function findWords(input: string) {
+  return WORDS.filter((word) => word.startsWith(input));
+}
+
+async function getEncrypter() {
+  const digits = await select({
+    message: 'Select encryption method:',
+    choices: [
+      { name: 'Word to number (4 digits)', value: 4 },
+      { name: 'Word to number (5 digits)', value: 5 },
+      { name: 'Word to number (6 digits)', value: 6 },
+    ],
+  });
+
+  while (true) {
+    const pass1 = await password(
+      {
+        message: 'Enter encryption password:',
+        mask: true,
+        validate: (value) => {
+          try {
+            new Encrypter(value, digits);
+            return true;
+          } catch (error: any) {
+            return error.message;
+          }
+        },
+      },
+      CLEAR_PROMPT
+    );
+
+    const pass2 = await password(
+      {
+        message: 'Confirm encryption password:',
+        mask: true,
+      },
+      CLEAR_PROMPT
+    );
+
+    if (pass1 === pass2) {
+      return new Encrypter(pass1, digits);
+    }
+
+    console.log(chalk.red(fig.cross), "Passwords don't match!");
+  }
+}
+
+function getWordChoices(isLast: boolean) {
+  return isLast
+    ? [
+        { name: 'Done', value: 'd' },
+        { name: 'Retry word', value: 'r' },
+      ]
+    : [
+        { name: 'Next word', value: 'n' },
+        { name: 'Retry word', value: 'r' },
+        { name: 'Cancel', value: 'q' },
+      ];
+}
+
+async function encrypt(enc: Encrypter, length: number) {
+  const color = chalk.cyan;
+
+  for (let n = 0; n < length; n++) {
+    const isLast = n + 1 >= length;
+    const num = `#${n + 1}`;
+
+    const word = await autocomplete(
+      {
+        message: `Enter word ${color(num)}:`,
+        source: async (input) =>
+          findWords(input || '').map((word) => ({ value: word })),
+      },
+      CLEAR_PROMPT
+    );
+
+    const encrypted = enc.encryptWord(word, n);
+
+    const sel = await select(
+      {
+        message: `Word ${color(num)}: ${color(word)}, encrypted: ${color(
+          encrypted
+        )}. Continue?`,
+        choices: getWordChoices(isLast),
+      },
+      CLEAR_PROMPT
+    );
+
+    if (sel === 'd') return true;
+    if (sel === 'q') return false;
+    if (sel === 'r') n--;
+  }
+}
+
+async function decrypt(enc: Encrypter, length: number) {
+  const color = chalk.cyan;
+
+  for (let n = 0; n < length; n++) {
+    const isLast = n + 1 >= length;
+    const num = `#${n + 1}`;
+
+    const encrypted = await input(
+      {
+        message: `Enter encrypted word ${color(num)}:`,
+        validate: (value) => {
+          try {
+            enc.decryptWord(value, n);
+            return true;
+          } catch (error: any) {
+            return error.message;
+          }
+        },
+      },
+      CLEAR_PROMPT
+    );
+
+    const decrypted = enc.decryptWord(encrypted, n);
+
+    const sel = await select(
+      {
+        message: `Word ${color(num)}: ${color(encrypted)}, decrypted: ${color(
+          decrypted
+        )}. Continue?`,
+        choices: getWordChoices(isLast),
+      },
+      CLEAR_PROMPT
+    );
+
+    if (sel === 'd') return true;
+    if (sel === 'q') return false;
+    if (sel === 'r') n--;
+  }
+}
+
+async function main() {
+  console.log('\n%s %s v%s.\n', chalk.green('*'), pkg.description, pkg.version);
+
+  const encryption = await select({
+    message: 'What would you like to do?',
+    choices: [
+      { name: 'Encrypt recovery phrase', value: true },
+      { name: 'Decrypt recovery phrase', value: false },
+    ],
+  });
+
+  const length = await select({
+    message: 'How long is your recovery phrase?',
+    choices: [
+      { name: '12 words', value: 12 },
+      { name: '24 words', value: 24 },
+    ],
+  });
+
+  const enc = await getEncrypter();
+
+  const handler = encryption ? encrypt : decrypt;
+  return handler(enc, length);
+}
+
+main()
+  .then(() => {
+    console.log(chalk.green(fig.tick), 'Done.');
+    process.exit(0);
+  })
+  .catch(() => {
+    console.log(chalk.red(fig.cross), 'Failed!');
+    process.exit(1);
+  });
